ec9351c8a4633bbede06776b9f70b53750cc96a3
Mizunashi Mana <mizunashi-mana@noreply.git>
2018-04-27 22:18:48 +0900

    Add backup command

--- lib/gitlabomni_manage_tools/command.rb
+++ lib/gitlabomni_manage_tools/command.rb
@@ -67,5 +67,10 @@ module GitLabOmnibusManage
     def notify_cronjob
       command_notify_cronjob
     end
+
+    desc 'backup [options]', 'backup command'
+    def backup
+      command_backup
+    end
   end
 end
